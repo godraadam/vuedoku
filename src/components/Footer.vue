b11f02f38ede9d6da6fd8239f9829a5be7ea6d12
godraadam <adamg@growmail.com>
2025-10-31 15:10:30 +0100

    amend untracked files

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="w-full flex py-3 px-4 justify-end absolute bottom-0">
+    <div class="flex gap-1 text-sm font-light">
+      made by
+      <a
+        href="https://godraadam.com"
+        class="hover:text-sky-500 text-center hover:underline transition-colors"
+        target="_blank"
+        >godraadam</a
+      >
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts"></script>
